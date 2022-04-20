950efaff77dbb2a638a5bf6b9c6c5544a6670ec2
Spacew1se <crod201@gmail.com>
2022-04-19 22:08:58 -0700

    Simplify playRound function
    Remove commented out code

--- javascript.js
+++ javascript.js
@@ -19,37 +19,6 @@ function playRound(playerSelection, computerSelection) {
     const winMessage = `You win! ${capitalizeFirstLetter(playerSelection)} beats ${computerSelection}.`;
     const loseMessage =  `You lose! ${capitalizeFirstLetter(computerSelection)} beats ${playerSelection}.`
 
-/*
-    let roundResult = null;
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'scissors') 
-            roundResult = winMessage;
-        else if (computerSelection === 'paper') 
-            roundResult = loseMessage;
-    }
-    else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') 
-            roundResult = winMessage;
-        else if (computerSelection === 'scissors') 
-            roundResult = loseMessage.`
-    }
-    else {
-        if (computerSelection === 'paper') 
-            roundResult = winMessage;
-        else if (computerSelection === 'rock') 
-            roundResult = loseMessage;
-    }
-*/
-
-/*
-    if (playerSelection === 'rock') 
-        roundResult = (computerSelection === 'scissors') ? winMessage : (computerSelection === 'paper') ? loseMessage : tieMessage;
-    else if (playerSelection === 'paper') 
-        roundResult = (computerSelection === 'rock') ? winMessage : (computerSelection === 'scissors') ? loseMessage : tieMessage
-    else 
-        roundResult = (computerSelection === 'paper') ? winMessage : (computerSelection === 'rock') ? loseMessage : tieMessage;
-*/
-
     const roundResult = (playerSelection === 'rock') ? ((computerSelection === 'scissors') ? winMessage : (computerSelection === 'paper') ? loseMessage : tieMessage) 
             : (playerSelection === 'paper') ? ((computerSelection === 'rock') ? winMessage : (computerSelection === 'scissors') ? loseMessage : tieMessage) 
             : ((computerSelection === 'paper') ? winMessage : (computerSelection === 'rock') ? loseMessage : tieMessage);

f1014656b5812b9ba7b4beeb9a5ab49301887bf8
Spacew1se <crod201@gmail.com>
2022-04-19 22:00:11 -0700

    Add playRound function
    Determine if the player or the computer wins. Convert user entry to
    lower case to sanitize. Add a helper function that capitalizes the first
    letter of a string for ease of output.
    
    This version contains commented out code for the playRound function
    showing alternate ways it can be written. I first used nested if/else
    statements but later shortened using ternary operators.

--- javascript.js
+++ javascript.js
@@ -2,10 +2,57 @@ function computerPlay() {
     
     let rand = Math.floor(Math.random() * 3);
 
-    let computerSelection = (rand === 0) ? 'Rock' : 
-        (rand === 1) ? 'Paper' : 'Scissors';
+    let computerSelection = (rand === 0) ? 'rock' : 
+        (rand === 1) ? 'paper' : 'scissors';
     
-        console.log(computerSelection);
+        return computerSelection;
 }
 
-computerPlay();
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+    const tieMessage = `It's a tie! We both chose ${playerSelection}.`;
+    const winMessage = `You win! ${capitalizeFirstLetter(playerSelection)} beats ${computerSelection}.`;
+    const loseMessage =  `You lose! ${capitalizeFirstLetter(computerSelection)} beats ${playerSelection}.`
+
+/*
+    let roundResult = null;
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') 
+            roundResult = winMessage;
+        else if (computerSelection === 'paper') 
+            roundResult = loseMessage;
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') 
+            roundResult = winMessage;
+        else if (computerSelection === 'scissors') 
+            roundResult = loseMessage.`
+    }
+    else {
+        if (computerSelection === 'paper') 
+            roundResult = winMessage;
+        else if (computerSelection === 'rock') 
+            roundResult = loseMessage;
+    }
+*/
+
+/*
+    if (playerSelection === 'rock') 
+        roundResult = (computerSelection === 'scissors') ? winMessage : (computerSelection === 'paper') ? loseMessage : tieMessage;
+    else if (playerSelection === 'paper') 
+        roundResult = (computerSelection === 'rock') ? winMessage : (computerSelection === 'scissors') ? loseMessage : tieMessage
+    else 
+        roundResult = (computerSelection === 'paper') ? winMessage : (computerSelection === 'rock') ? loseMessage : tieMessage;
+*/
+
+    const roundResult = (playerSelection === 'rock') ? ((computerSelection === 'scissors') ? winMessage : (computerSelection === 'paper') ? loseMessage : tieMessage) 
+            : (playerSelection === 'paper') ? ((computerSelection === 'rock') ? winMessage : (computerSelection === 'scissors') ? loseMessage : tieMessage) 
+            : ((computerSelection === 'paper') ? winMessage : (computerSelection === 'rock') ? loseMessage : tieMessage);
+
+    return roundResult;
+}

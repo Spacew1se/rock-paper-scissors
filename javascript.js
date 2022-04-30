03f3a30afd6a2f837aa8639defa5d841efe73597
Spacew1se <crod201@gmail.com>
2022-04-29 20:37:50 -0700

    Implement playAgain and resetGame functions
    When the game is over a button is displayed so the user can play again
    Clicking the button will remove the nodes that display the winner, and
    reset the scores to 0.

--- javascript.js
+++ javascript.js
@@ -13,46 +13,87 @@ function playRound(playerChoice) {
     computerSelection = getComputerSelection();
     playerSelection = playerChoice.target.id;
 
+    
     const tieMessage = `It's a tie! We both chose ${playerSelection}.`;
     const winMessage = `You win! ${capitalizeFirstLetter(playerSelection)} beats ${computerSelection}.`;
     const loseMessage =  `You lose! ${capitalizeFirstLetter(computerSelection)} beats ${playerSelection}.`;
+
     const roundResult = (playerSelection === 'rock') ? ((computerSelection === 'scissors') ? winMessage : (computerSelection === 'paper') ? loseMessage : tieMessage) 
             : (playerSelection === 'paper') ? ((computerSelection === 'rock') ? winMessage : (computerSelection === 'scissors') ? loseMessage : tieMessage) 
             : ((computerSelection === 'paper') ? winMessage : (computerSelection === 'rock') ? loseMessage : tieMessage);
 
-    const results = document.querySelector('.rndResult');
-    results.textContent = roundResult;
-    results.style.backgroundColor = 'purple';
 
-    subRes = roundResult.slice(4,7);
-    if (subRes === 'win') playerRoundsWon++;
-    else if (subRes === 'los') cpuRoundsWon++;
+    if (roundsPlayed++ === 0) {
+        const rndResult = document.createElement('div');
+        rndResult.classList.add('rndResult');
+        const score = document.querySelector('.score');
+        results.insertBefore(rndResult, score);
+    }
+
+    rndResult = document.querySelector('.rndResult');
+    rndResult.textContent = roundResult;
+    rndResult.style.backgroundColor = 'purple';
+
+    subRes = roundResult.slice(4,5);
+    if (subRes === 'w') playerRoundsWon++;
+    else if (subRes === 'l') cpuRoundsWon++;
         
+    displayScore();
+    
+    if(playerRoundsWon === WINCOND || cpuRoundsWon === WINCOND) {
+        gameOver();
+    } 
+    
+}
+
+function gameOver() {
+    const gameResult = (playerRoundsWon === WINCOND) ? "Congratulations, you won the game!" 
+        : "You lost the game. Better luck next time!";
+
+    const displayWinner = document.createElement('div');  
+    displayWinner.classList.add('winner');
+    displayWinner.textContent = gameResult;
+    results.appendChild(displayWinner);
+    
+    playAgain();
+}
+
+function playAgain() {
+    const again = document.createElement('button');
+    again.classList.add('again')
+    again.textContent = "Play Again"
+    results.appendChild(again)
+    again.addEventListener('click', resetGame)
+}
+
+function resetGame() {
+    playerRoundsWon = 0;
+    cpuRoundsWon = 0;
+    roundsPlayed = 0;
+    const rmWinDisplay = document.querySelector('.winner');
+    rmWinDisplay.parentNode.removeChild(rmWinDisplay);
+    const rmButton = document.querySelector('.again');
+    rmButton.parentNode.removeChild(rmButton);
+    const rmRndDisplay = document.querySelector('.rndResult');
+    rmRndDisplay.parentNode.removeChild(rmRndDisplay)
+    displayScore();
+}
+
+function displayScore() {
     const playerScore = document.querySelector('.plyrRounds');
     const cpuScore = document.querySelector('.cpuRounds');
 
     playerScore.textContent = `Player: ${playerRoundsWon}`;
     cpuScore.textContent = `Opponent: ${cpuRoundsWon}`;
-    
+
 }
 
-
-function game() {
-    
-    let gameResult = "";
-
-    alert("Let's play rock, paper, scissors! Best out of 5 rounds wins.");
-
-    gameResult = (playerRoundsWon > cpuRoundsWon) ? "Congratulations, you won the game!" 
-        : (playerRoundsWon < cpuRoundsWon) ? "You lost the game. Better luck next time!" : "We tied! What are the odds of that?";
-
-    console.log(gameResult);
-}
-
-
+const WINCOND = 5;
 let playerRoundsWon = 0;
 let cpuRoundsWon = 0;
+let roundsPlayed = 0;
 
+const results = document.querySelector('.results')
 const buttons = document.querySelectorAll('.btns');
 buttons.forEach(btn => btn.addEventListener('click', playRound))
 
